3578b0c14a74c6a6150c833f8370ef67105135a5
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-05 13:28:47 +0530

    [Shantanu] Added UC_2_diceRoll

---
+++ UC_2_diceRoll/UC_2_diceRoll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC_2_diceRoll
+{
+    class UC_2_diceRoll
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int diceRoll = random.Next(0, 7);
+            Console.WriteLine("You got " + diceRoll+ " in dice roll.");
+        }
+    }
+}
