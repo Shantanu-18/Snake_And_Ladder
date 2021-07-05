6e2ed28d3db020927642888fb519d8a9a158897b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-05 13:22:45 +0530

    Add project files.

---
+++ Snake_And_Ladder/UC_1_gameStarted.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_And_Ladder
+{
+    class UC_1_gameStarted
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WELCOME TO SNAKE AND LADDER GAME :)");
+            Console.WriteLine("Single Player Game is started Your at position 0.");
+            Console.ReadKey();
+        }
+    }
+}
